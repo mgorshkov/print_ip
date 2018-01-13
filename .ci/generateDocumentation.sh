5d1f1a74cb5475d974ee1170c985bfcba6c02ec7
Mikhail Gorshkov <mikhail.gorshkov@ntprog.com>
2018-01-13 11:46:16 +0300

    OTUS task 4 -extra sh file

--- .ci/generateDocumentation.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-DOXYFILE=doc/Doxyfile
-
-################################################################################
-##### Generate the Doxygen code documentation and log the output.          #####
-echo 'Generating Doxygen code documentation...'
-# Redirect both stderr and stdout to the log file AND the console.
-doxygen $DOXYFILE 2>&1 | tee doxygen.log
